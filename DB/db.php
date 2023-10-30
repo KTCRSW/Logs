1ba1e0862f562397b4532b03842eddf9aa8bcb92
unknown <kittichdev@gmail.com>
2023-10-30 11:16:37 +0700

    Commit

--- DB/db.php
+++ DB/db.php
@@ -1,11 +1,9 @@
 <?php 
 
     //logDataBase
-
-
         $dbhost = 'localhost';
         $dbroot = 'root';
-        $dbpass = '';
+        $dbpass = 'kittichai';
         $dbname = 'logs_it_network';
 
         $db = mysqli_connect($dbhost, $dbroot, $dbpass, $dbname)or die(mysqli_connect_error());
---
+++ Insert.php
@@ -0,0 +1,13 @@
+<?php 
+
+    require './DB/db.php';
+    include './Asset/Header.php';
+
+
+?>
+
+<form action="App/Create.php">
+
+    <input type="text" name="caseNumber">
+
+</form>
