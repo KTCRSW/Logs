91fc4a38b04853d3841115172ab2175dae3f652a
unknown <hornesmonkut@gmail.com>
2023-10-30 13:10:01 +0700

    gg

---
+++ Login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+  <div class="min-h-screen sm:flex sm:flex-row mx-0 justify-center">
+      <div class="flex-col flex  self-center p-10 sm:max-w-5xl xl:max-w-2xl  z-10">
+        <div class="self-start hidden lg:flex flex-col  text-white">
+          <img src="" class="mb-3">
+        
+    
+        </div>
+      </div>
+      <div class="flex justify-center self-center   z-10">
+        <div class="p-12 shadow-md bg-white mx-auto rounded-2xl w-100 ">
+            <div class="mb-4">
+              <h3 class="font-semibold text-2xl text-gray-800">Sign In </h3>
+            
+            </div>
+            <div class="space-y-5">
+                        <div class="space-y-2">
+                              <label class="text-sm font-medium text-gray-700 tracking-wide">Email</label>
+              <input class=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400" type="" placeholder="mail@gmail.com">
+              </div>
+                          <div class="space-y-2">
+              <label class="mb-5 text-sm font-medium text-gray-700 tracking-wide">
+                Password
+              </label>
+              <input class="w-full content-center text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400" type="" placeholder="Enter your password">
+            </div>
+              <div class="flex items-center justify-between">
+           
+             
+            </div>
+            <div>
+              <button type="submit" class="w-full flex justify-center bg-green-400  hover:bg-green-500 text-gray-100 p-3  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500">
+                Sign in
+              </button>
+            </div>
+            </div>
+            <div class="pt-5 text-center text-gray-400 text-xs">
+              <span>
+            
+              
+            </div>
+        </div>
+      </div>
+  </div>
+</div>
+</body>
+</html>
