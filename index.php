24cd983f40fe8ce0ec11f190e4c8a2df82273b64
unknown <hornesmonkut@gmail.com>
2023-10-31 13:03:58 +0700

    table

--- index.php
+++ index.php
@@ -21,10 +21,10 @@ include './Asset/SideNav.php';
                 </svg>
             </div>
             <input type="search" id="default-search"
-                class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search Mockups, Logos..." required>
+                class="block w-full p-4 pl-10 text-sm text-black border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="ค้นหา" required>
             <button type="submit"
-                class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">ค้นหา</button>
         </div>
     </form>
     
@@ -81,29 +81,32 @@ include './Asset/SideNav.php';
                 while($row = mysqli_fetch_assoc($query)):
                 
                 ?>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <tr class="bg-white border-b dark:bg-white dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-bold text-gray-900 whitespace-nowrap dark:text-black">
                         <?php echo $row['LOGS_CASE_NUMBER'];?>
                     </th>
-                    <td class="px-6 py-4">
+                    <td class="px-6 py-4 text-black">
                         <?php echo $row['LOGS_DATE'];?>
 
                     </td>
-                    <td class="px-6 py-4">
+                    <td class="px-6 py-4 text-black">
                         <?php echo $row['LOGS_LOCATION'];?>
 
                     </td>
-                    <td class="px-6 py-4">
+                    <td class="px-6 py-4 text-black">
                         <?php echo $row['LOGS_TECHNICIANS'];?>
 
                     </td>
 
+
+
+                    
                     <td class="px-6 py-4 text-right">
-                        <a href="#" data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" style="text-decoration:none;color:orange;"><i class="fa-regular fa-pen-to-square"></i>Edit</a>
+                        <a href="#" data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" style="text-decoration:none;color:orange;"><i class="fa-regular fa-pen-to-square"></i>แก้ไข</a>
                         <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative w-full max-w-md max-h-full">
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-100">
             <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
@@ -121,7 +124,7 @@ include './Asset/SideNav.php';
             </div>
             <div class="md:w-2/3">
                 <input
-                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-400"
+                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white  focus:border-green-400"
                     id="inline-full-name" type="text" value="000000001" name="__caseNumber" required>
             </div>
         </div>
