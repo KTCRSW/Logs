1b8da25075d8dc1b86e642f3bc6d89e28a928967
unknown <kittichdev@gmail.com>
2023-10-31 10:51:02 +0700

    Backend

--- index.php
+++ index.php
@@ -32,12 +32,12 @@ include './Asset/SideNav.php';
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="px-6 py-3">
-                        Case Number
+                        เลขแจ้งเคส
                     </th>
                     <th scope="col" class="px-6 py-3">
                         <div class="flex items-center">
-                            Case Date
-                            <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
+                            วันที่แจ้งดำเนินงาน
+                            <a href="#" disabled><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                     <path
                                         d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
@@ -46,8 +46,8 @@ include './Asset/SideNav.php';
                     </th>
                     <th scope="col" class="px-6 py-3">
                         <div class="flex items-center">
-                            Case Location
-                            <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
+                            สถานที่เข้างาน
+                            <a href="#" disabled><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                     <path
                                         d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
@@ -56,8 +56,8 @@ include './Asset/SideNav.php';
                     </th>
                     <th scope="col" class="px-6 py-3">
                         <div class="flex items-center">
-                            Case Technician
-                            <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
+                            ช่างผู้ทำงาน
+                            <a href="#" disabled><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                     <path
                                         d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
